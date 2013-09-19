3ddf62e6066b78e360cbb0500cdea4d5486bc9fc
txu0393 <txu0393@gmail.com>
2013-09-19 12:08:46 -0400

    Added documentation. Made minor changes

--- PortAuthority/PortAuthority.cs
+++ PortAuthority/PortAuthority.cs
@@ -11,6 +11,9 @@ using PacketDotNet;
 
 namespace PortAuthority
 {
+    /// <summary>
+    /// The PortAuthority class is used to perform various packet capturing functions and web queries associated with a specified network interface and Student Life's Netcenter's port_authority.php
+    /// </summary>
     public class PortAuthority
     {
         #region Fields
@@ -18,6 +21,7 @@ namespace PortAuthority
         private string url;
         private ICaptureDevice idev;
         #endregion
+
         #region Properties
         public string MACAddress
         {
@@ -30,7 +34,12 @@ namespace PortAuthority
             private set;
         }
         #endregion
+
         #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the PortAuthority class that focuses on the NetworkInterface object that is passed to this constructor.
+        /// </summary>
+        /// <param name="n"></param>
         public PortAuthority(NetworkInterface n)
         {
             ni = n;
@@ -39,7 +48,12 @@ namespace PortAuthority
             IPAddress = GetIP();
         }
         #endregion
+
         #region Private Methods
+        /// <summary>
+        /// Gets the IPv4 Address associated with the network interface
+        /// </summary>
+        /// <returns>A string representing the IPv4 address in xxx.xxx.xxx.xxx format where xxx is an integer in the range of [0,256]</returns>
         private string GetIP()
         {
             foreach (UnicastIPAddressInformation ipi in ni.GetIPProperties().UnicastAddresses)
@@ -52,6 +66,18 @@ namespace PortAuthority
             }
             return "0.0.0.0";
         }
+
+        /// <summary>
+        /// Returns a query string using the parameters as the field values
+        /// </summary>
+        /// <param name="rm">Room number</param>
+        /// <param name="jack">Jack name as stated in the room</param>
+        /// <param name="nswitch">Switch name</param>
+        /// <param name="port">The name of the port used on the switch</param>
+        /// <param name="gb">True if the port supports Gigabit speeds. False otherwise</param>
+        /// <param name="user">Student Affairs username for authentication</param>
+        /// <param name="pass">Student Affairs password for authentication</param>
+        /// <returns>A query string with the fields filled out by the values given in the parameters</returns>
         private string GenerateQueryString(string rm, string jack, string nswitch, string port, bool gb, string user, string pass)
         {
             var query = new StringBuilder();
@@ -71,6 +97,23 @@ namespace PortAuthority
             query.Append(pass);
             return query.ToString();
         }
+
+        /// <summary>
+        /// Converts a query string to an equivalent array of bytes
+        /// </summary>
+        /// <param name="query">The query string</param>
+        /// <returns>A byte array representing the given query string</returns>
+        private byte[] ConvertQueryStringToBytes(string query)
+        {
+            return Encoding.UTF8.GetBytes(query);
+        }
+
+        /// <summary>
+        /// Sends a POST request to Netcenter with data given by the parameter postData
+        /// Then calls the private method <c>ReadNetCenter</c> to obtain the response message and returns it as a string.
+        /// </summary>
+        /// <param name="postData">Data to be posted to Netcenter</param>
+        /// <returns>A string representing the response from Netcenter</returns>
         private string WriteNetCenter(byte[] postData)
         {
             var req = WebRequest.Create(url);
@@ -80,9 +123,15 @@ namespace PortAuthority
             var dstream = req.GetRequestStream();
             dstream.Write(postData, 0, postData.Length);
             dstream.Close();
-            return ReadNetCenter(req);
+            return GetWebRequestReponse(req);
         }
-        private string ReadNetCenter(WebRequest wr)
+
+        /// <summary>
+        /// Gets the response message from the WebRequest object passed in the parameter
+        /// </summary>
+        /// <param name="wr">The WebRequest object to be interrogated</param>
+        /// <returns>A string with the reponse message from the given WebRequest</returns>
+        private string GetWebRequestReponse(WebRequest wr)
         {
             string resp;
             var wresp = wr.GetResponse();
@@ -94,6 +143,7 @@ namespace PortAuthority
             wresp.Close();
             return resp;
         }
+
         private bool SetupCaptureDevice()
         {
             bool success = false;
@@ -109,6 +159,7 @@ namespace PortAuthority
             }
             return success;
         }
+
         private bool GetSwitchInformation(ref string sname, ref string port, ref bool gigabit)
         {
             var lldp = CaptureLLDPPacket();
@@ -123,6 +174,7 @@ namespace PortAuthority
             port = port.Split('/')[2];
             return true;
         }
+
         private LLDPPacket CaptureLLDPPacket()
         {
             idev.Filter = "ether dst 01:80:c2:00:00:0e";
@@ -130,12 +182,14 @@ namespace PortAuthority
             var packet = Packet.ParsePacket(rawpacket.LinkLayerType, rawpacket.Data);
             return (LLDPPacket)packet.Extract(typeof(LLDPPacket));
         }
+
         private void CloseCaptureDevice()
         {
             if (idev != null)
                 idev.Close();
         }
         #endregion
+
         #region Public Methods
         public void RefeshIP()
         {
@@ -156,7 +210,7 @@ namespace PortAuthority
                 return "Unable to capture packet: Check the connection and try again";
             CloseCaptureDevice();
             string query = GenerateQueryString(rm, jack, nswitch, port, gb, user, pass);
-            var postData = Encoding.UTF8.GetBytes(query);
+            var postData = ConvertQueryStringToBytes(query);
             return WriteNetCenter(postData);
         }
         #endregion
